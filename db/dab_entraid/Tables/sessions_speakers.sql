a58106ed22db464555ad4033f2fa3555926ead48
Davide Mauri <damauri@microsoft.com>
2024-03-06 09:02:04 -0800

    added constraint names

--- db/dab_entraid/Tables/sessions_speakers.sql
+++ db/dab_entraid/Tables/sessions_speakers.sql
@@ -2,8 +2,8 @@ CREATE TABLE [web].[sessions_speakers] (
     [session_id] INT NOT NULL,
     [speaker_id] INT NOT NULL,
     PRIMARY KEY CLUSTERED ([session_id] ASC, [speaker_id] ASC),
-    FOREIGN KEY ([session_id]) REFERENCES [web].[sessions] ([id]),
-    FOREIGN KEY ([speaker_id]) REFERENCES [web].[speakers] ([id])
+    CONSTRAINT FK1 FOREIGN KEY ([session_id]) REFERENCES [web].[sessions] ([id]),
+    CONSTRAINT FK2 FOREIGN KEY ([speaker_id]) REFERENCES [web].[speakers] ([id])
 );
 GO
 
