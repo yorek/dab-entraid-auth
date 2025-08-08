08dd72754528cc23998288900980856e2fc2d87d
Davide Mauri <damauri@microsoft.com>
2025-08-08 11:38:09 -0700

    removed files

--- authConfig.js
+++
@@ -1,8 +0,0 @@
-var authConfig = {
-    clientId: "41e8bb4d-7ff6-437a-8740-2dfd598dcf2b",
-    authority: "https://login.microsoftonline.com/36a71206-0015-4845-a10b-02a03ed530bd",
-}
-
-var requestScopes = [
-    "api://33d3ffac-ed9a-4fbe-b79f-5bc96713e860/Endpoints.Access"
-]
